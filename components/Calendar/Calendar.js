941874404f507134adb3410f821d87fefed9ade0
stephmen <stephmen@gmail.com>
2022-12-04 19:09:09 -0500

    removed duplicated header from the calendar

--- components/Calendar/Calendar.js
+++ components/Calendar/Calendar.js
@@ -15,31 +15,22 @@ const Calendar = (props) => {
   return(
     <CalendarStyle>
     <div className='container'>
+   
+    
+    
+    
+    
     <header>
     <div className="col">
       <h3>Date</h3></div>
-    <div className="col"><h3>Évènement</h3></div>
-    <div className="col"><h3> Lieu</h3></div>
-    <div className="col"><h3>Info</h3></div>
+    <div className="col"><h3>Spectacle</h3></div>
+    <div className="col"><h3> Endroit</h3></div>
+    <div className="col"><h3> + Info</h3></div>
     </header>
     
     
     
     
-<<<<<<< HEAD
-=======
-    <header>
-    <div className="col">
-      <h3>Date</h3></div>
-    <div className="col"><h3>Évènement</h3></div>
-    <div className="col"><h3> Lieu</h3></div>
-    <div className="col"><h3>Info</h3></div>
-    </header>
-    
-    
-    
-    
->>>>>>> 53375ec5a8c767015161139f4f91da552c2a303d
     {props.posts.map(
       ({ _id, title = '', slug = '', publishedAt, endroit
       = '' }) =>
@@ -52,21 +43,16 @@ const Calendar = (props) => {
         <div className="col"> {endroit} </div>
         <div className="col">
         <Link key={_id} href="/spectacles/[slug]" as={`/spectacles/${slug.current}`}>
-<<<<<<< HEAD
         <img src="/LesAmisCrop/sue3.png"  height="50"/>
-=======
-        Info
->>>>>>> 53375ec5a8c767015161139f4f91da552c2a303d
         </Link>
         </div>
         </div>
         ))}
     
-<<<<<<< HEAD
+
     </div>
-=======
+
    
->>>>>>> 53375ec5a8c767015161139f4f91da552c2a303d
     </CalendarStyle>
     )
     
