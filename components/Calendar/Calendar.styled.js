afb7c61bf15c0ab03f4300b002239da1f56db18f
stephmen <stephmen@gmail.com>
2023-08-04 15:38:18 -0400

    nouveau fond d ecran

--- components/Calendar/Calendar.styled.js
+++ components/Calendar/Calendar.styled.js
@@ -1,88 +1,52 @@
 import styled from 'styled-components';
+
 export const CalendarStyle = styled.div`
-// <<<<<<< HEAD
-
-.container {
-  margin: 1rem 1rem;
-  background-color: #eff971;
-  
-}
-
-header, .row, .col {
-  border: 0px solid;
-  border-top: 0;
-  display: flex;
-  justify-content: center
-   
-}
-header {
-  margin-top: 1rem;
-  border: 2px solid
-}
-.col {
-  flex: 1
-}
-.row {
-  border-bottom: 0px solid;
-  padding-top: 2rem;
-  
-
-}   
-.row:hover, .row:hover > * {
-  cursor: pointer;
-  background-color: #f7f7f7;
+  .container {
+    margin: 1rem 1rem;
+    background-color: #eff971;
   }
 
-@media (max-width: 768px) {
-    .container {
-      margin: 1rem 0rem
-      padding: 4rem;
-    }
-
-    header {
-      margin-top: 0.5rem;
-      border: 1px solid;
-    }
-    
-    .col:nth-child(4) {
-      display: none;
-    }
-
-    .col:nth-child(2) {
-    padding-left: 1rem;
-  }
-    .col:nth-child(3) {
-    padding-left: 1rem;
-  }
   .row {
-  cursor: pointer;
-  padding-top: 2rem ;
+    display: flex;
+    flex-direction: column;
+    border-bottom: 1px solid #ccc;
+    padding: 1rem;
+    cursor: pointer;
   }
 
   .row:hover {
-  background-color: #f7f7f7;
+    background-color: #f7f7f7;
   }
 
-.row:hover, .row:hover > * {
-  cursor: pointer;
-  background-color: #f7f7f7;
+  .date {
+    font-weight: bold;
+    font-size: 1.2rem;
   }
-}
 
+  .location {
+    margin-top: 0.5rem;
+    font-size: 1rem;
+  }
 
+  .image {
+    margin-top: 1rem;
+    display: flex;
+    justify-content: center;
+  }
+
+  .image img {
+    height: 50px;
+  }
+
+  @media (max-width: 768px) {
+    .container {
+      margin: 1rem 0rem;
+      padding: 4rem;
+    }
+
+    .row {
+      border-bottom: none;
+      padding-bottom: 2rem;
+    }
+  }
 `;
-
-
-
-
-
-
-
-
-
-
-
-
-  
-
-
