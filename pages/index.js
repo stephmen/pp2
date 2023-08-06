b48f4889cec18ed013de47a96732cfcf4600c340
Stephane-Menard <stephane.menard@ocean.ibm.com>
2023-08-05 22:02:42 -0400

    page mobile on load fix

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,38 @@
-// import PageOne from './pageOne'
-// import Head from 'next/head'
+import { useEffect } from 'react';
+import Head from 'next/head';
+import { isMobile } from 'react-device-detect';
+import Router from 'next/router';
 
+import PageOne from './pageOne';
+
+export default function Home() {
+  useEffect(() => {
+    if (isMobile) {
+      Router.push('/mobile-home');
+    }
+  }, []);
+
+  return (
+    <>
+      <Head>
+        <title>Fanfare PourPour</title>
+        {/* <link rel="icon" href="/favicon.ico" /> */}
+      </Head>
+      <PageOne key="page-one" />
+    </>
+  );
+}
+// import PageOne from './pageOne';
+// import Head from 'next/head';
+// import { isMobile } from 'react-device-detect';
+// import Router from 'next/router';
 
 // export default function Home() {
+//   if (isMobile) {
+//     Router.push('/mobile-home');
+//     return null;
+//   }
+
 //   return (
 //     <div >
 //       <Head>
@@ -11,26 +41,5 @@
 //       </Head>
 //       <PageOne />      
 //     </div>
-//   )
+//   );
 // }
-import PageOne from './pageOne';
-import Head from 'next/head';
-import { isMobile } from 'react-device-detect';
-import Router from 'next/router';
-
-export default function Home() {
-  if (isMobile) {
-    Router.push('/mobile-home');
-    return null;
-  }
-
-  return (
-    <div >
-      <Head>
-        <title>Fanfare PourPour</title>
-        {/* <link rel="icon" href="/favicon.ico" /> */}
-      </Head>
-      <PageOne />      
-    </div>
-  );
-}
