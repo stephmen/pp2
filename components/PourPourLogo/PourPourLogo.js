6f8808e6b44cf5fb4f4529ec766a1add288d7c6e
Stephane-Menard <stephane.menard@ocean.ibm.com>
2023-09-06 11:31:39 -0400

    Premiere Page

--- components/PourPourLogo/PourPourLogo.js
+++ components/PourPourLogo/PourPourLogo.js
@@ -5,7 +5,7 @@ export default function PourPourLogo (props) {
 
 return(  
 <PPLogo {...props}>
-<img className="logo" src="/pp4/nom_pourpour.png" />
+<img className="logo" src="/pp4/nom_pourpour_2.png" />
 </PPLogo>
 )
 }
