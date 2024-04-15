784704b073780998b48796d2ec133f35c55a6a53
Stephane-Menard <stephane.menard@ocean.ibm.com>
2024-04-14 21:15:40 -0400

    chaging presse pour get getInitialProps

--- pages/presse.jsx
+++ pages/presse.jsx
@@ -1,32 +1,30 @@
-import React from 'react'
-import Press from '../components/Press'
+import React from 'react';
+import Press from '../components/Press';
 import Extraitpresse from '../components/Extraitpresse';
-import { Header } from '../components'
+import { Header } from '../components';
 import sanityClient from "../client";
 import groq from "groq";
   
 
-    export default function Presse(props) {
-    
-    
-    return(
+const Presse = (props) => {
+  return (
     <div>
-      
-    <Press />
-    <Extraitpresse {...props} />
+      <Press />
+      <Extraitpresse {...props} />
     </div>
+  );
+};
 
-)}
-export const getStaticProps = async () => {
-    const client = sanityClient.withConfig({ apiVersion: '2021-06-07' });
-  
-    const posts = await client.fetch(groq`
-      *[_type == "extraitpresse" ]
-    `);
-    console.log('Fetched posts:', posts); // Log the fetched posts
-  
-    return {
-      props: { posts },
-    };
+Presse.getInitialProps = async () => {
+  const client = sanityClient.withConfig({ apiVersion: '2021-06-07' });
+  const posts = await client.fetch(groq`
+    *[_type == "extraitpresse"]
+  `);
+  console.log('Fetched posts:', posts); // Log the fetched posts
+
+  return {
+    posts
   };
+};
 
+export default Presse;
