39feb8ca135d6cc10196c22eaa2697454d226a69
Stephane-Menard <stephane.menard@ocean.ibm.com>
2024-05-28 22:58:37 -0400

    changer presse

--- components/Press/Press.js
+++ components/Press/Press.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Image from "next/image";
 import styled from "styled-components";
 import { StyledPress } from "./Press.styled";
 import Background from '../BackGround';
@@ -7,7 +8,7 @@ const Disque = styled.div`
   background-color: #e6e3d1;
   width: 100%;
   padding: 20px;
-  box-sizing: border-box; /* Include padding in width calculation */
+  box-sizing: border-box;
 `;
 
 const Title = styled.p`
@@ -50,25 +51,25 @@ export default function Press() {
           <Title>EXTRAITS DE PRESSE</Title>
 
           <Commentaire>
-            <img alt="Richard Desjardins" src="image1.jpg" />
+            <Image alt="Richard Desjardins" src="/image1.jpg" width={100} height={60} />
             <Quote>
-              <span>Il y a de ça trente ans. La première édition de la fanfare Pourpour. À l'époque, il n'était pas nécessaire de savoir bien jouer. Il suffisait d'aimer être ensemble. J'avais remarqué une adorable petite femme qui y jouait du cor. J'ai donc appris un peu de trombone pour être plus près d'elle. J'ai eu un enfant d'elle. Aujourd'hui, l'orchestre joue merveilleusement bien. Viva Pourpour!</span>
+              <span>Il y a de ça trente ans. La première édition de la fanfare Pourpour. À l&apos;époque, il n&apos;était pas nécessaire de savoir bien jouer. Il suffisait d&apos;aimer être ensemble. J&apos;avais remarqué une adorable petite femme qui y jouait du cor. J&apos;ai donc appris un peu de trombone pour être plus près d&apos;elle. J&apos;ai eu un enfant d&apos;elle. Aujourd&apos;hui, l&apos;orchestre joue merveilleusement bien. Viva Pourpour!</span>
               <span className="author">Richard Desjardins, 2004</span>
             </Quote>
           </Commentaire>
 
           <Commentaire>
-            <img alt="Patrick Duval" src="image2.jpg" />
+            <Image alt="Patrick Duval" src="/image2.jpg" width={100} height={60} />
             <Quote>
-              <span>«Plein de choses en eux m'ont séduit... Leur complicité véritable, la diversité des instruments. La possibilité de les voir subitement déclamer un poème, ce lien musical entre le continent nord-américain et l'Europe... Cette fanfare a un côté très romantique. Elle offre un voyage. On embarque. Et quand on ressort, on est bien.»</span>
+              <span>&laquo;Plein de choses en eux m&apos;ont séduit... Leur complicité véritable, la diversité des instruments. La possibilité de les voir subitement déclamer un poème, ce lien musical entre le continent nord-américain et l&apos;Europe... Cette fanfare a un côté très romantique. Elle offre un voyage. On embarque. Et quand on ressort, on est bien.&raquo;</span>
               <span className="author">Patrick Duval, directeur de Musiques de Nuit, Bordeaux, France (2008)</span>
             </Quote>
           </Commentaire>
 
           <Commentaire>
-            <img alt="Georges Harrison" src="image3.jpg" />
+            <Image alt="Georges Harrison" src="/image3.jpg" width={100} height={60} />
             <Quote>
-              <span>«The most enjoyable music I've heard in a long time.»</span>
+              <span>&laquo;The most enjoyable music I&apos;ve heard in a long time.&raquo;</span>
               <span className="author">Georges Harrison, avril 2000</span>
             </Quote>
           </Commentaire>
